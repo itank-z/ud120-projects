cafad9700dc5d1f2cfe904a87a2582e04c536bfa
itank <ankit.it97@gmail.com>
2019-02-28 20:41:45 +0530

    Support Vector Machine

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,7 +23,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+
+from sklearn import svm
+import time
+clf = svm.SVC(kernel="rbf", C = 10000)
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time.time()
+clf.fit(features_train, labels_train)
+print "\ntraining time : ",(time.time()-t0)," s"
+
+t0 = time.time()
+pred = clf.predict(features_test)
+print "\nprediction time : ",(time.time()-t0)," s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print "\naccuracy is ", accuracy
+
+no_chris_emails = 0
+
+for i in pred:
+	if(i==1):
+		no_chris_emails += 1
+
+print "\nno of emails by chris : ", no_chris_emails
 
 #########################################################
 
