a94f621f5601dd126726772737415d404aa6a2b3
itank <ankit.it97@gmail.com>
2019-02-28 20:50:51 +0530

    Validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,5 +28,16 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.model_selection import train_test_split
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print "accuracy : ", accuracy_score(labels_test, pred)
 
